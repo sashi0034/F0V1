01743f65423a71ede49da1f90cf07a9a25980c46
sashi <satoshi3104.nishi@gmail.com>
2025-02-09 16:43:30 +0900

    ウィンドウクラス作成

--- F0V1/main.cpp
+++ F0V1/main.cpp
@@ -1,10 +1,43 @@
 #include "Windows.h"
 #include <iostream>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+namespace
 {
-	OutputDebugString(L"Hello, World!\n");
-	std::getchar();
-	return 0;
+    LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+    {
+        if (msg == WM_DESTROY)
+        {
+            PostQuitMessage(0);
+            return 0;
+        }
+
+        return DefWindowProc(hwnd, msg, wparam, lparam);
+    }
+
+    class WindowCore
+    {
+    public:
+        void Init()
+        {
+            m_windowClass.cbSize = sizeof(WNDCLASSEX);
+            m_windowClass.lpfnWndProc = static_cast<WNDPROC>(WindowProcedure);
+            m_windowClass.lpszClassName = L"F0";
+            m_windowClass.hInstance = GetModuleHandle(nullptr);
+            RegisterClassEx(&m_windowClass);
+        }
+
+    private:
+        WNDCLASSEX m_windowClass{};
+    };
+
+    class EngineCore
+    {
+    };
 }
 
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    OutputDebugString(L"Hello, World!\n");
+    std::getchar();
+    return 0;
+}
