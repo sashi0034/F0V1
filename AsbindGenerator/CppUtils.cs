2e6f479c478e5b8dd2d51dcf179cacec40a3db4f
sashi <satoshi3104.nishi@gmail.com>
2025-04-04 20:19:09 +0900

    CppAst 導入

---
+++ AsbindGenerator/CppUtils.cs
@@ -0,0 +1,17 @@
+﻿#nullable enable
+
+namespace AsbindGenerator;
+
+public static class CppUtils
+{
+    public static string[] GetHeaderFiles(string absolutePath)
+    {
+        if (!Path.IsPathRooted(absolutePath))
+            throw new ArgumentException("Provided path is not an absolute path.");
+
+        if (!Directory.Exists(absolutePath))
+            throw new DirectoryNotFoundException("Specified directory does not exist.");
+
+        return Directory.GetFiles(absolutePath, "*.h", SearchOption.AllDirectories);
+    }
+}
--- AsbindGenerator/Program.cs
+++ AsbindGenerator/Program.cs
@@ -1,2 +1,60 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using CppAst;
+using Irony.Parsing;
+
+namespace AsbindGenerator;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Usage: program.exe <absolute_path>");
+            return;
+        }
+
+        string rootPath = args[0];
+
+        var headers = CppUtils.GetHeaderFiles(rootPath);
+
+        Console.WriteLine(Directory.GetCurrentDirectory());
+        foreach (var header in headers)
+        {
+            var content = File.ReadAllText(header);
+
+            // Parse a C++ files
+            var option = new CppParserOptions().ConfigureForWindowsMsvc();
+            option.IncludeFolders.Add(Path.GetDirectoryName(header));
+            option.IncludeFolders.Add(rootPath);
+            option.AdditionalArguments.Add("-std=c++20");
+
+            var compilation = CppParser.Parse(content, option);
+
+            // Print diagnostic messages
+            foreach (var message in compilation.Diagnostics.Messages)
+            {
+                Console.WriteLine(message);
+            }
+
+            // Print All enums
+            foreach (var cppEnum in compilation.Enums)
+            {
+                Console.WriteLine(cppEnum);
+            }
+
+            // Print All functions
+            foreach (var cppFunction in compilation.Functions)
+            {
+                Console.WriteLine(cppFunction);
+            }
+
+            // Print All classes, structs
+            foreach (var cppClass in compilation.Classes)
+            {
+                Console.WriteLine(cppClass);
+            }
+        }
+    }
+}
