4376875c9a5a1559920035855bde7c49158cdf64
sashi <satoshi3104.nishi@gmail.com>
2025-04-05 11:49:55 +0900

    Fix typo

--- F0V1/ZG/KeyboardInput.h
+++ F0V1/ZG/KeyboardInput.h
@@ -8,29 +8,29 @@ namespace ZG
 }
 
 #define ASAPI_VALUE_CLASS(name, flags) \
-    static inline std::vector<std::function<void(asbind20::value_class<name>)>> s_aspi_bindHandlers{}; \
+    static inline std::vector<std::function<void(asbind20::value_class<name>)>> s_asapi_bindHandlers{}; \
     static auto RegisterScript(asIScriptEngine* engine) { \
         auto bind = asbind20::value_class<name>(engine, #name, flags) \
             .behaviours_by_traits(); \
-        for (const auto& handler : s_aspi_bindHandlers) \
+        for (const auto& handler : s_asapi_bindHandlers) \
         { \
             handler(bind); \
         } \
     } \
-    using aspi_BindTarget = name;
+    using asapi_BindTarget = name;
 
 #define ASAPI_CLASS_METHOD(decl, method_name) \
     struct asapi_##method_name \
     { \
         asapi_##method_name() \
         { \
-            s_aspi_bindHandlers.push_back([](asbind20::value_class<aspi_BindTarget> bind) \
+            s_asapi_bindHandlers.push_back([](asbind20::value_class<asapi_BindTarget> bind) \
             { \
-                bind.method(decl, &aspi_BindTarget::method_name); \
+                bind.method(decl, &asapi_BindTarget::method_name); \
             }); \
         } \
     }; \
-    static inline asapi_##method_name s_aspi_scriptBind_##method_name{};
+    static inline asapi_##method_name s_asapi_scriptBind_##method_name{};
 
 namespace ZG
 {
